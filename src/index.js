aa153eefbfd4f0ebc9f82c52a6c3b6e0996d2367
Dan G <ddgroleau@gmail.com>
2021-03-01 07:16:49 -0700

    complete api connection

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-import React, { useState, Component, useEffect } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-
-console.log('Hello Candles!');
-
-const getListings = async () => {
-    const URL = `https://openapi.etsy.com/v2/listings/active?api_key=`;
-    const KEY = process.env.REACT_APP_KEYSTRING;
-    const request = await fetch(URL+KEY, 
-      {
-      header: {
-      'Access-Control-Allow-Origin':'*',
-    }
-  });
-    const response = await request;
-    console.log(response.json());
+const account = {
+USER_URL: "https://openapi.etsy.com/v2/users/CANdles.js?callback=getData&api_key=",
+LISTINGS_URL: "https://openapi.etsy.com/v2/shops/CANdles5280/listings/active.js?callback=getData&api_key=",
+KEY: process.env.REACT_APP_KEYSTRING,
+getUser() {
+  document.getElementById('user').setAttribute('src', (this.USER_URL + this.KEY));
+},
+getListings() {
+  document.getElementById('listings').setAttribute('src', (this.LISTINGS_URL + this.KEY));
 }
-getListings()
+}
+account.getUser();
+account.getListings();
