1156f4afbeec5de05c2e7dc61a914068fe26ce88
Dan G <ddgroleau@gmail.com>
2021-03-14 17:31:37 -0600

    update mobile responsivity

--- Client/src/components/HomeSection.js
+++ Client/src/components/HomeSection.js
@@ -7,7 +7,7 @@ import video2 from "../assets/videos/video-2.mp4"
 function HomeSection() {
     return (
         <div className='home-container'>
-            <video src={video2} autoPlay loop muted playsInLine />
+            <video src={video2} autoPlay loop muted playsInline />
             
             <h1>CANdles</h1>
             <p>Earth-friendly wax + recycled craft beer cans.</p>
