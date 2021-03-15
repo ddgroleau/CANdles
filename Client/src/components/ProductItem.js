c13128c7422d990df5a2320e76335448ec65ec00
Dan G <ddgroleau@gmail.com>
2021-03-15 07:53:19 -0600

    update responsivity

--- Client/src/components/ProductItem.js
+++ Client/src/components/ProductItem.js
@@ -5,7 +5,7 @@ function ProductItem(props) {
     return (
         <div className='product__li__wrapper'>
             <li className='product__li'>
-                        <img src={props.imageUrl} alt="product" srcSet={props.imageUrl + "**0.5**x"}
+                        <img src={props.imageUrl} alt="product"
                         className='product__li__img' />
                     <div className="product__li__info">
                         <h6 className='producer__li__text'>{props.producer}</h6>
